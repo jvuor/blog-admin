5b1e9faefa84b4abe00c8c96caf8a5fa96d71238
jvuor <jussi.vuorinen@gmail.com>
2018-07-26 11:56:26 +0300

    bug fix to BLOGEDIT reducer

--- src/reducers/blogReducer.js
+++ src/reducers/blogReducer.js
@@ -10,14 +10,16 @@ const reducer = (store = null, action) => {
   if(action.type === 'BLOGINIT') {
     return action.data
   }
-  if(action.type === 'BLOGDELETE') {
+  if(action.type === 'BLOGEDIT') {
+    console.log('edit', action)
     const editedIndex = store.findIndex(blog => blog.id === action.id)
     const newBlogData = {
+      ...store[editedIndex],
       title: action.title,
       content: action.content,
-      sticky: action.sticky,
-      ...store[editedIndex]
+      sticky: action.sticky
     }
+    console.log(newBlogData)
 
     return store.map(blog => blog.id === newBlogData.id? newBlogData : blog)
   }
